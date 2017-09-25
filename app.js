938991a2d8ebe39570b41f7bb1a8ac0579675156
John Mubang <jmubang@gmail.com>
2017-09-25 03:01:45 +0000

    added mLab db to app.js. fixed password input in login.ejs and register.ejs.

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var commentRoutes    = require("./routes/comments"),
     authRoutes       = require("./routes/index");
 
 
-mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
+// mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
+mongoose.connect("mongodb://JJ:fepquest@ds147864.mlab.com:47864/camp");
 mongoose.Promise = global.Promise;
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
